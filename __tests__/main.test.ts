f28b6a23ccd86c802534e8ee8633ebb8a8827c5f
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-04-15 19:26:47 +1000

    Initial ts action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,18 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  expect(1).toBe(1)
+  // process.env['INPUT_TOKEN'] = process.env.token || 'github PAT'
+  // process.env['INPUT_ISSUE-NUMBER'] = '1'
+  // process.env['INPUT_REPOSITORY'] = 'docs'
+  // const np = process.execPath
+  // const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  // const options: cp.ExecFileSyncOptions = {
+  //   env: process.env
+  // }
+  // console.log(cp.execFileSync(np, [ip], options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,49 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {inspect} from 'util'
+
+const ORG = 'KittyCAD'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('token')
+    const issueNumber = Number(core.getInput('issue-number'))
+    const repo = core.getInput('repository')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`issue: ${issueNumber}, repo: ${repo}`)
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(token)
+
+    const {data: projects} = await octokit.rest.projects.listForOrg({
+      org: ORG
+    })
+    const project = projects.find(({name}) => name === 'All Tasks')
+
+    if (!project) throw new Error("Couldn't get the 'All Tasks' project")
+
+    core.debug(`Project: ${inspect(project)}`)
+
+    const {data: columns} = await octokit.rest.projects.listColumns({
+      project_id: project.id
+    })
+    core.debug(`Columns: ${inspect(columns)}`)
+
+    const column = columns.find(({name}) => name === 'No Status')
+
+    if (!column) throw new Error("The 'No Status' column couldn't be found.")
+
+    const {data: issue} = await octokit.rest.issues.get({
+      owner: ORG,
+      repo,
+      issue_number: issueNumber
+    })
+
+    const {data: card} = await octokit.rest.projects.createCard({
+      column_id: column.id,
+      content_id: issue.id,
+      content_type: 'Issue'
+    })
+    core.debug(`Card: ${inspect(card)}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
