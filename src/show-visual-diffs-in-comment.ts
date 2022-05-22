157538d3fdc8cd5a38414d6b93c6f90950dd76ba
Kurt Hutten <k.hutten@protonmail.ch>
2022-05-22 16:42:28 +1000

    Reorganise ts action repo (#28)
    Adding an action to comment and diff images generated by playwright, however repo needs some reorganising to support multiple actions in this repo.

---
+++ src/show-visual-diffs-in-comment.ts
@@ -0,0 +1,49 @@
+import * as core from '@actions/core'
+import * as filestack from 'filestack-js'
+import * as github from '@actions/github'
+import {globby} from 'globby'
+import {inspect} from 'util'
+import {readFile} from 'node:fs/promises'
+
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token')
+    const filestackKey = core.getInput('filestack-key')
+
+    const client = filestack.init(filestackKey)
+    const paths = await globby('**/*diff.png')
+    const uploadPromises = paths.map(async path => {
+      const file = await readFile(path)
+      const response = await client.upload(file)
+      core.debug(`upload response for ${path}: ${inspect(response)}`)
+      let summaryPath = path.split('/').pop() || ''
+      summaryPath = summaryPath?.replace('diff.png', '').split('-').join(' ')
+      return `
+<details>
+  <summary>${summaryPath}</summary>
+      ${path}
+</details>
+
+![${path}](${response.url})`
+    })
+    const mdLines = await Promise.all(uploadPromises)
+
+    const commentBody = [
+      '### Ch-ch-ch-ch-changes',
+      'Turn and face the strange\n',
+      ...mdLines
+    ].join('\n')
+    const octokit = github.getOctokit(token)
+
+    await octokit.rest.issues.createComment({
+      issue_number: github?.context?.payload?.pull_request?.number || 0,
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      body: commentBody
+    })
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
