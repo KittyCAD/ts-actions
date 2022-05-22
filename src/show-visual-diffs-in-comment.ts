b5ff2737e57c298aeace82c3cf171e02cc859a62
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-05-23 07:12:04 +1000

    start migration to gcloud

--- src/show-visual-diffs-in-comment.ts
+++ src/show-visual-diffs-in-comment.ts
@@ -1,12 +1,35 @@
 import * as core from '@actions/core'
 import * as filestack from 'filestack-js'
+import {Storage} from '@google-cloud/storage'
 import {globby} from 'globby'
 import {inspect} from 'util'
 import {readFile} from 'node:fs/promises'
 
+// The ID of your GCS bucket
+// const bucketName = 'your-unique-bucket-name'
+
+// The path to your file to upload
+// const filePath = 'path/to/your/file'
+
+// // The new ID for your GCS file
+// const destFileName = 'your-new-file-name'
+
+// Imports the Google Cloud client library
+
+// Creates a client
+const storage = new Storage()
+
+async function uploadFile(bucketName: string, filePath: string): Promise<void> {
+  const response = await storage.bucket(bucketName).upload(filePath)
+
+  console.log(`${filePath} uploaded to ${bucketName}`)
+  console.log(`gcloud response: ${response}`)
+}
+
 async function run(): Promise<void> {
   try {
     const filestackKey = core.getInput('filestack-key')
+    const bucketName = core.getInput('bucket-key')
 
     const client = filestack.init(filestackKey)
     let paths = await globby('**/*diff.png')
@@ -17,6 +40,9 @@ async function run(): Promise<void> {
       core.debug(`upload response for ${path}: ${inspect(response)}`)
       let summaryPath = path.split('/').pop() || ''
       summaryPath = summaryPath?.replace('diff.png', '').split('-').join(' ')
+
+      await uploadFile(bucketName, path)
+
       return `
 <details>
   <summary>${summaryPath}</summary>
@@ -36,7 +62,6 @@ async function run(): Promise<void> {
     ].join('\n')
 
     core.setOutput('body', commentBody)
-
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
