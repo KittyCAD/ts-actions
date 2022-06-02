da9d9975fd75b7bb0f1f47a7ca60ef67c8b82e6f
Kurt Hutten <k.hutten@protonmail.ch>
2022-06-02 14:58:29 +1000

    fix entrypoint.sh

--- install-kittycad/entrypoint.sh
+++ install-kittycad/entrypoint.sh
@@ -1,12 +1,11 @@
 # Export the sha256sum for verification.
-export KITTYCAD_CLI_SHA256="101a7e812d9e8c19ac034b0f57a7b8ee85543e22db78552b64987631ce7ad470"
+export KITTYCAD_CLI_SHA256="a3d8e6471745abbf134f58c0c666a3d114d9646d56e60231a20a233a44536f7f"
 
 
 # Download and check the sha256sum.
-curl -fSL "https://dl.kittycad.io/releases/cli/v0.0.9/cli-linux-arm64" -o "/usr/local/bin/kittycad" \
+curl -fSL "https://dl.kittycad.io/releases/cli/v0.0.9/cli-freebsd-amd64" -o "/usr/local/bin/kittycad" \
 	&& echo "${KITTYCAD_CLI_SHA256}  /usr/local/bin/kittycad" | sha256sum -c - \
 	&& chmod a+x "/usr/local/bin/kittycad"
-
 
 echo "kittycad cli installed!"
 
