45cbd7391c5edbf10b611ec9d94e246ac1b3c32d
Kurt Hutten <k.hutten@protonmail.ch>
2022-10-10 07:22:39 +1100

    autogen-readme-list (#112)
    * autogen-readme-list
    
    * fix test action
    
    * fix typo
    
    * stage files
    
    * comment diff instead of commiting
    
    * copy paste error
    
    * tweak comment
    
    * comment tweak

---
+++ src/autogen-readme-list/autogen-readme-list.ts
@@ -0,0 +1,97 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import fsp from 'node:fs/promises'
+import {inspect} from 'util'
+
+async function reWriteMarkdown(token: string, path: string) {
+  const octokit = github.getOctokit(token)
+  const markdown = await fsp.readFile(path, 'ascii')
+  console.log(markdown)
+  const lines = markdown.split('\n')
+  const tagsMap: {[tag: string]: {repo: string; url: string}[]} = {}
+
+  lines.forEach(line => {
+    if (line.includes('start-autogen-list')) {
+      const tag = (line.match(/tag=(.+)\s-->/) || [])[1]
+      tagsMap[tag] = []
+    }
+  })
+
+  const repoTopicResponse: {
+    organization: {
+      repositories: {
+        nodes: {
+          name: string
+          url: string
+          repositoryTopics: {
+            nodes: {
+              topic: {
+                name: string
+              }
+            }[]
+          }
+        }[]
+      }
+    }
+  } = await octokit.graphql(
+    `
+        query {
+            organization(login: "KittyCAD") {
+            repositories(first: 50){ 
+              nodes {
+                name
+                url
+                repositoryTopics(first: 10) {
+					        nodes {
+                    topic {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+          `
+  )
+
+  // loop over repositories
+  repoTopicResponse.organization.repositories.nodes.forEach(repo => {
+    // loop over topics
+    repo.repositoryTopics.nodes.forEach(({topic}) => {
+      // check if topic is in tagsMap
+      if (topic.name in tagsMap) {
+        // add repo to tagsMap
+        tagsMap[topic.name].push({
+          repo: repo.name,
+          url: repo.url
+        })
+      }
+    })
+  })
+
+  let newMarkdown = markdown
+  for (const [tag, repos] of Object.entries(tagsMap)) {
+    const regExTemplate = `<!--\\sstart-autogen-list\\stag=(${tag})\\s-->([\\S\\s])*?<!--\\send-autoGen-list\\s-->`
+    const regEx = RegExp(regExTemplate, 'gim')
+    const newLines = repos
+      .sort((a, b) => (a.repo > b.repo ? 1 : -1))
+      .map(({url, repo}) => `- [${repo}](${url})`)
+    newLines.unshift(`<!-- start-autogen-list tag=${tag} -->`)
+    newLines.push('<!-- end-autogen-list -->')
+
+    newMarkdown = newMarkdown.replace(regEx, newLines.join('\n'))
+  }
+  core.debug(`tagsMap: ${inspect(tagsMap)}`)
+  core.debug(`markdown: ${inspect(newMarkdown)}`)
+
+  fsp.writeFile(path, newMarkdown)
+}
+
+function main() {
+  const token = core.getInput('github-token')
+  const path = core.getInput('path')
+  reWriteMarkdown(token, path)
+}
+
+main()
---
+++ src/autogen-readme-list/demo-readme.md
@@ -0,0 +1,26 @@
+<img width="933" alt="KittyCAD_header" src="https://user-images.githubusercontent.com/19377312/185000988-28372052-5fd4-468e-aba7-bd23b3e50140.png">
+
+# :cat: KittyCAD :cat:
+
+<sub>A place to 'pin' links and repositories for quick grab.</sub>
+
+<img src="https://img.shields.io/badge/KittyCAD-helpful--repos-cyan" alt="Helpful Repos">
+
+- [Support](https://github.com/KittyCAD/support) or [support@kittycad.io](mailto:support@kittycad.io)
+- [CLI (Command Line Interface)](https://github.com/KittyCAD/cli)
+- [Documentation](https://github.com/KittyCAD/documentation)
+<!-- - [Litterbox - Sample Scripts](https://github.com/KittyCAD/litterbox) -->
+
+<img src="https://img.shields.io/badge/KittyCAD-github--actions-yellow" alt="Useful Github Actions">
+
+<!-- start-autogen-list tag=github-action -->
+- [action-install-cli](https://github.com/KittyCAD/action-install-cli)
+<!-- end-autogen-list -->
+
+<img src="https://img.shields.io/badge/KittyCAD-api--client--libs-ff69b4" alt="API Client Libs">
+
+<!-- start-autogen-list tag=api-client -->
+- [kittycad.go](https://github.com/KittyCAD/kittycad.go)
+- [kittycad.py](https://github.com/KittyCAD/kittycad.py)
+- [kittycad.ts](https://github.com/KittyCAD/kittycad.ts)
+<!-- end-autogen-list -->
