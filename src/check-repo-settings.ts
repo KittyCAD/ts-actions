82ba838e7b3eefc1d8a5b50a6b716507c89634ef
Kurt Hutten <k.hutten@protonmail.ch>
2022-10-21 08:37:20 +1100

    auto fix repo settings (#132)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/check-repo-settings.ts
+++ src/check-repo-settings.ts
@@ -54,63 +54,156 @@ async function main(): Promise<void> {
     }
       `
   )
-  const mergeRuleMessage = [
-    '### Bad merge rules',
-    'Merge rules for the repo should be:',
-    'mergeCommitAllowed: `false`, rebaseMergeAllowed: `false`, squashMergeAllowed: `true`',
-    'The following repos have different merge rule settings:',
-    ''
-  ]
-  const initialMergeRuleMessageLength = mergeRuleMessage.length
 
-  const protectedBranchMessage = [
-    '### Bad protected branch rules',
-    'The `main` branch should be protected, requiring pull requests',
-    "The following repos don't have the correct settings for `main`:",
-    ''
-  ]
-  const initialProtectedBranchMessageLength = protectedBranchMessage.length
+  interface Info {
+    showMessage: boolean
+    overallMessage: string
+    fixedMessage: string
+    fixedList: string[]
+    errorHeaderMessage: string
+    errorList: string[]
+  }
+
+  const mergeRuleInfo: Info = {
+    showMessage: false,
+    overallMessage: [
+      '### Bad merge rules',
+      'Merge rules for the repo should be:',
+      'mergeCommitAllowed: `false`, rebaseMergeAllowed: `false`, squashMergeAllowed: `true`'
+    ].join('\n'),
+    fixedMessage: 'The following were fixed',
+    fixedList: [],
+    errorHeaderMessage: 'Unable to fix to following:',
+    errorList: []
+  }
+
+  const protectedBranchInfo: Info = {
+    showMessage: false,
+    overallMessage: [
+      '### Bad protected branch rules',
+      'The `main` branch should be protected, requiring pull requests'
+    ].join('\n'),
+    fixedMessage: 'The following were fixed',
+    fixedList: [],
+    errorHeaderMessage: 'Unable to fix to following:',
+    errorList: []
+  }
   const repos: string[] = []
-  repoRulesQuery.organization.repositories.nodes.forEach(repo => {
-    const isPrivatePrivateRepo = repo.name.startsWith('_')
-    if (!isPrivatePrivateRepo) {
-      repos.push(repo.name)
-    }
-    const hasCorrectMergeRules =
-      !repo.mergeCommitAllowed &&
-      !repo.rebaseMergeAllowed &&
-      repo.squashMergeAllowed
-    if (
-      !hasCorrectMergeRules &&
-      !ignoreRepos.includes(repo.name) &&
-      !isPrivatePrivateRepo
-    ) {
-      mergeRuleMessage.push(
-        `- [ ] [${repo.name}](https://github.com/KittyCAD/${repo.name}/settings)`
-      )
-    } else {
-      console.log(`${repo.name} good merge rules`)
-    }
-
-    const isMainBranchProtected = repo.branchProtectionRules.nodes.some(
-      ({allowsForcePushes, pattern, requiresApprovingReviews}) => {
-        return (
-          pattern === 'main' && !allowsForcePushes && !requiresApprovingReviews
-        )
+  await Promise.all(
+    repoRulesQuery.organization.repositories.nodes.map(async repo => {
+      const hasCorrectMergeRules =
+        !repo.mergeCommitAllowed &&
+        !repo.rebaseMergeAllowed &&
+        repo.squashMergeAllowed
+      if (!hasCorrectMergeRules && !ignoreRepos.includes(repo.name)) {
+        mergeRuleInfo.showMessage = true
+        try {
+          await octokit.rest.repos.update({
+            owner: 'KittyCAD',
+            repo: repo.name,
+            allow_merge_commit: false,
+            allow_rebase_merge: false,
+            allow_squash_merge: true,
+            use_squash_pr_title_as_default: true,
+            squash_merge_commit_title: 'PR_TITLE'
+          })
+          mergeRuleInfo.fixedList.push(
+            `- [${repo.name}](https://github.com/KittyCAD/${repo.name})`
+          )
+        } catch (error) {
+          console.error(error)
+          mergeRuleInfo.errorList.push(
+            `- [${repo.name}](https://github.com/KittyCAD/${repo.name})`
+          )
+        }
+      } else {
+        console.log(`${repo.name} good merge rules`)
       }
-    )
-    if (
-      !isMainBranchProtected &&
-      !ignoreRepos.includes(repo.name) &&
-      !isPrivatePrivateRepo
-    ) {
-      protectedBranchMessage.push(
-        `- [ ] [${repo.name}](https://github.com/KittyCAD/${repo.name}/settings/branches)`
-      )
-    } else {
-      console.log(`${repo.name} has main protected`)
-    }
-  })
+      let isMainBranchProtected = false
+      let canFetchSettings = false
+      let settingsData: any = {}
+      try {
+        const {data} = await octokit.rest.repos.getBranchProtection({
+          repo: repo.name,
+          owner: 'KittyCAD',
+          branch: 'main'
+        })
+        isMainBranchProtected = !!data.required_pull_request_reviews
+        console.log(
+          `Is ${repo.name} main branch protected? ${isMainBranchProtected}`
+        )
+        canFetchSettings = true
+        settingsData = data
+        const userRestrictions = data.restrictions?.users
+          .map(user => user.login || '')
+          .filter(Boolean)
+        const teamsRestrictions = data.restrictions?.teams
+          .map(team => team.slug || '')
+          .filter(Boolean)
+        const appsRestrictions = data.restrictions?.apps
+          .map(app => app.slug || '')
+          .filter(Boolean)
+        settingsData.restrictions =
+          userRestrictions || teamsRestrictions || appsRestrictions
+            ? {
+                users: userRestrictions,
+                teams: teamsRestrictions,
+                apps: appsRestrictions
+              }
+            : null
+        const statusChecksStrict = data.required_status_checks?.strict
+        const statusChecksContexts = data.required_status_checks?.contexts || []
+        const statusChecks =
+          data.required_status_checks?.checks.map(check => ({
+            context: check.context,
+            app_id: check.app_id
+          })) || []
+        const requiredStatusChecks = data.required_status_checks
+          ? {
+              strict: statusChecksStrict,
+              contexts: statusChecksContexts,
+              checks: statusChecks
+            }
+          : null
+        settingsData.required_status_checks = requiredStatusChecks
+      } catch (error) {
+        console.log(error)
+        isMainBranchProtected = false
+      }
+
+      if (!canFetchSettings) {
+        protectedBranchInfo.showMessage = true
+        protectedBranchInfo.errorList.push(
+          `- [${repo.name}](https://github.com/KittyCAD/${repo.name})`
+        )
+      } else if (!isMainBranchProtected && !ignoreRepos.includes(repo.name)) {
+        protectedBranchInfo.showMessage = true
+        try {
+          await octokit.rest.repos.updateBranchProtection({
+            owner: 'KittyCAD',
+            repo: repo.name,
+            branch: 'main',
+            required_pull_request_reviews: {
+              required_approving_review_count: 0
+            },
+            enforce_admins: settingsData?.enforce_admins?.enabled,
+            required_status_checks: settingsData?.required_status_checks,
+            restrictions: settingsData?.restrictions
+          })
+          protectedBranchInfo.fixedList.push(
+            `- [${repo.name}](https://github.com/KittyCAD/${repo.name})`
+          )
+        } catch (error) {
+          console.error(error)
+          protectedBranchInfo.errorList.push(
+            `- [${repo.name}](https://github.com/KittyCAD/${repo.name})`
+          )
+        }
+      } else {
+        console.log(`${repo.name} has main protected`)
+      }
+    })
+  )
 
   const dependabotYmlFetches = await Promise.all(
     repos.map(async repo => {
@@ -143,14 +236,38 @@ async function main(): Promise<void> {
     }
   })
 
-  const mergeRuleMessageSection =
-    initialMergeRuleMessageLength < mergeRuleMessage.length
-      ? mergeRuleMessage.join('\n')
-      : ''
-  const protectedBranchMessageSection =
-    initialProtectedBranchMessageLength < protectedBranchMessage.length
-      ? protectedBranchMessage.join('\n')
-      : ''
+  const mergeRuleMessageSection = mergeRuleInfo.showMessage
+    ? [
+        mergeRuleInfo.overallMessage,
+        mergeRuleInfo.fixedList.length
+          ? [mergeRuleInfo.fixedMessage, ...mergeRuleInfo.fixedList].join('\n')
+          : '',
+        mergeRuleInfo.errorList.length
+          ? [mergeRuleInfo.errorHeaderMessage, ...mergeRuleInfo.errorList].join(
+              '\n'
+            )
+          : ''
+      ].join('\n')
+    : ''
+
+  const protectedBranchMessageSection = protectedBranchInfo.showMessage
+    ? [
+        protectedBranchInfo.overallMessage,
+        protectedBranchInfo.fixedList.length
+          ? [
+              protectedBranchInfo.fixedMessage,
+              ...protectedBranchInfo.fixedList
+            ].join('\n')
+          : '',
+        protectedBranchInfo.errorList.length
+          ? [
+              protectedBranchInfo.errorHeaderMessage,
+              ...protectedBranchInfo.errorList
+            ].join('\n')
+          : ''
+      ].join('\n')
+    : ''
+
   const dependabotBulletMessageSection =
     initialDependabotBulletMessageLength < dependabotBulletMessage.length
       ? dependabotBulletMessage.join('\n')
@@ -164,8 +281,10 @@ async function main(): Promise<void> {
     mergeRuleMessageSection ||
     protectedBranchMessageSection ||
     dependabotBulletMessageSection
+
   core.setOutput('isproblems', !!isProblems)
   core.setOutput('body', issueBody)
+  console.log(issueBody)
 }
 
 main()
